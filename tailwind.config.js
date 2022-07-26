7aaa4caf02986bec5cacdc569d498b43b561fabd
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-07-26 23:21:11 +0100

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
-const colors = require('tailwindcss/colors')
-
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
