f4b69add7829c7cc12c4f8e8cbbe8f304984de32
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-07-26 23:26:53 +0100

    adding colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+// tailwind.config.js
+const { emerald } = require('tailwindcss/colors')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     "./index.html",
@@ -6,7 +10,27 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        amber: colors.amber,
+        emerald: colors.emerald,
+        neutral: colors.neutral,
+        slate: colors.slate,
+        zinc: colors.zinc,
+        stone: colors.stone,
+        orange: colors.orange,
+        lime: colors.lime,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        blue: colors.blue,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuschsia: colors.fuschsia,
+        pink: colors.pink,
+        rose: colors.rose
+      }
+    },
   },
   variants: {
     extend: {},
