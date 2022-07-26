6b257bc2b1113b4fc0a753ab69db03960ba3c69c
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-07-26 23:17:52 +0100

    config

--- src/components/nav.vue
+++ src/components/nav.vue
@@ -1,13 +1,12 @@
 <template>
-<h1>test</h1>
     <div class="w-full h-20 bg-green-900 flex pt-4 pb-4 lg:pl-40 lg:pr-40 items-center pl-4 pr-4 justify-between">
       <div>
         <h1 class="text-2xl text-white"><b>Task</b><span class="text-light">Track</span></h1>
       </div>
       <div>
         <router-link class="pr-8 pl-8 text-white text-opacity-70 hover:text-opacity-100 duration-100 bg-neutral-800 ml-4 mr-4 pt-2 pb-2 rounded-md bg-opacity-50 hover:bg-opacity-100" to="/">Home</router-link>
-         <router-link v-if="!loggedIn" class="pr-8 pl-8 text-white text-opacity-70 hover:text-opacity-100 duration-100 bg-neutral-800 ml-4 mr-4 pt-2 pb-2 rounded-md bg-opacity-50 hover:bg-opacity-100" to="/board">Sign In / Register</router-link>
-        <router-link v-if="loggedIn" class="pr-8 pl-8 text-white text-opacity-70 hover:text-opacity-100 duration-100 bg-neutral-800 ml-4 mr-4 pt-2 pb-2 rounded-md bg-opacity-50 hover:bg-opacity-100" to="/board">Your Board</router-link>
+         <router-link v-if="!loggedIn" class="pr-8 pl-8 text-white text-opacity-70 hover:text-opacity-100 duration-100 ml-4 mr-4 pt-2 pb-2 rounded-md bg-opacity-50 hover:bg-opacity-100" to="/board">Sign In / Register</router-link>
+        <router-link v-if="loggedIn" class="pr-8 pl-8 text-white text-opacity-70 hover:text-opacity-100 duration-100 ml-4 mr-4 pt-2 pb-2 rounded-md bg-opacity-50 hover:bg-opacity-100" to="/board">Your Board</router-link>
       </div>
     </div>
 </template>
