20fe0528f9ba762ff9e29529cbd9840a2059fbff
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-07-27 14:30:20 +0100

    tailwind fixes

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="fixed top-0 w-screen h-8 z-50 flex items-center pl-8 pt-8">
-        <h1 class="opacity-70 hover:opacity-100 duration-100 relative test">🡨 Go Back</h1>
+        <h1 class="opacity-70 bg-gray-300 rounded-md p-2 hover:opacity-100 duration-100 relative before:bg-green-400 before:absolute before:w-full before:h-full before:left-0 before:blur-xl before:opacity-0 hover:before:opacity-60 before:duration-200 hover:cursor-pointer">🡨 <span class="ml-4">Go Back</span></h1>
     </div>
     <div id="logincomponent" class="fixed top-0 w-screen h-screen z-40 bg-gray-100 flex items-center justify-center">
         <div class="bg-gray-300 p-8 rounded-lg">
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
-  purge: [],
+   // These paths are just examples, customize them to match your project structure
+
+   purge: [
+
+    './public/**/*.html',
+
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
