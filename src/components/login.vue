a0d9380fe97c831dc6d1d658de6b3837ac4d026a
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-07-27 14:11:12 +0100

    login page

---
+++ src/components/login.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="fixed top-0 w-screen h-8 z-50 flex items-center pl-8 pt-8">
+        <h1 class="opacity-70 hover:opacity-100 duration-100 relative test">🡨 Go Back</h1>
+    </div>
+    <div id="logincomponent" class="fixed top-0 w-screen h-screen z-40 bg-gray-100 flex items-center justify-center">
+        <div class="bg-gray-300 p-8 rounded-lg">
+            <h1 class="text-3xl font-semibold text-gray-700">Login to your account</h1>
+            <div class="mt-8">
+                <input v-model="username" class="rounded-md w-full p-2 pl-2 pr-4" placeholder="Username">
+                <input v-model="password" class="rounded-md w-full p-2 pl-2 pr-4 mt-3" placeholder="Password">
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['isLoggedIn'],
+        data(){
+            return{
+                username: "",
+                password: ""
+            }
+        },
+        methods:{
+            logIn(){
+                this.$root.isUserLoggedIn = true
+                this.$emit('userHasLoggedIn')
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/BoardPg.vue
+++ src/views/BoardPg.vue
@@ -1,5 +1,28 @@
 <template>
+  <login v-if="!isLoggedIn" @userHasLoggedIn="logIn()"/>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import login from '@/components/login.vue'
+
+export default {
+  name: 'board',
+  data(){
+    return{
+      isLoggedIn: this.$root.isUserLoggedIn
+    }
+  },
+  methods:{
+    userHasLoggedIn(){
+      this.$root.isUserLoggedIn = true
+    }
+  },
+  components: {
+    login
+  }
+}
+
+</script>
