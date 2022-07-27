35a95653e0a9a7c1d421aee3b7b48dfc276e1e61
VLCTY <88147999+devVelocity@users.noreply.github.com>
2022-07-27 14:47:41 +0100

    made login seperate from board page

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,13 +1,17 @@
 <template>
-    <div class="fixed top-0 w-screen h-8 z-50 flex items-center pl-8 pt-8">
-        <h1 class="opacity-70 bg-gray-300 rounded-md p-2 hover:opacity-100 duration-100 relative before:bg-green-400 before:absolute before:w-full before:h-full before:left-0 before:blur-xl before:opacity-0 hover:before:opacity-60 before:duration-200 hover:cursor-pointer">🡨 <span class="ml-4">Go Back</span></h1>
+    <div class="fixed top-0 w-screen h-8 z-[25] flex items-center pl-8 pt-8">
+        <router-link to="/" class=" bg-gray-300 bg-opacity-50  text-gray-900  rounded-md p-2 opacity-100 duration-100 relative hover:cursor-pointer">🡨 <span class="ml-4">Go Back</span></router-link>
     </div>
-    <div id="logincomponent" class="fixed top-0 w-screen h-screen z-40 bg-gray-100 flex items-center justify-center">
+    <div id="logincomponent" class="fixed top-0 w-screen h-screen z-[20] bg-gray-100 flex items-center justify-center">
         <div class="bg-gray-300 p-8 rounded-lg">
-            <h1 class="text-3xl font-semibold text-gray-700">Login to your account</h1>
+            <h1 class="text-3xl font-semibold text-gray-700">Login / Register</h1>
             <div class="mt-8">
                 <input v-model="username" class="rounded-md w-full p-2 pl-2 pr-4" placeholder="Username">
                 <input v-model="password" class="rounded-md w-full p-2 pl-2 pr-4 mt-3" placeholder="Password">
+                <div class="w-full flex mt-8 gap-3">
+                    <h1 class="w-full text-center bg-gray-800 bg-opacity-100 hover:bg-green-800 text-white  rounded-md p-2 opacity-100 duration-100 relative hover:cursor-pointer">Sign In</h1>
+                    <h1 class="w-full text-center bg-gray-800 bg-opacity-100 hover:bg-green-800 text-white  rounded-md p-2 opacity-100 duration-100 relative hover:cursor-pointer">Register</h1>
+                </div>
             </div>
         </div>
     </div>
@@ -19,7 +23,9 @@
         data(){
             return{
                 username: "",
-                password: ""
+                password: "",
+                errorMSG: "",
+                registerMode:false
             }
         },
         methods:{
@@ -32,5 +38,7 @@
 </script>
 
 <style scoped>
-
+.translateZ:before{ 
+    z-index: -1
+}
 </style>
